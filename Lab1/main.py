293335f1a5e4754e58cebdefc7a1b9efc2e8b772
m1st0rm <mishaboy333@gmail.com>
2023-05-24 14:26:53 +0300

    Added calculate func in main.py

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,2 +1,16 @@
+def calculate(x, y, operation):
+    if operation == 'add':
+        return x+y
+    elif operation == 'sub':
+        return x-y
+    elif operation == 'mul':
+        return x*y
+    elif operation == 'div':
+        if y == 0:
+            print('Division by zero is impossible')
+        else:
+            return x/y
+
+
 if __name__ == '__main__':
     print('Hello World!')
