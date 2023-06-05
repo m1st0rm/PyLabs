fb2d0da60cd31973568f666b172a417ae83ddad5
m1st0rm <mishaboy333@gmail.com>
2023-06-05 13:58:47 +0300

    class Storage defined with all required methods

--- Lab2/Task2/storage.py
+++ Lab2/Task2/storage.py
@@ -26,3 +26,91 @@ def value_to_string(value):
         value_list.append(str(value_part))
 
     return ' '.join(value_list) + ' '
+
+
+class Storage:
+
+    def __init__(self):
+        self.user_dict = dict[str, set]()
+        self.active_user = None
+
+    def user_list(self):
+        return list(self.user_dict)
+
+    def add(self, *keys):
+        if self.active_user == None:
+            return False
+
+        container = self.user_dict[self.active_user]
+        container |= set(keys)
+        return True
+
+    def remove(self, key):
+        if self.active_user == None:
+            return False
+
+        container = self.user_dict[self.active_user]
+
+        if key not in container:
+            return False
+
+        container.remove(key)
+        return True
+
+    def find(self, *keys):
+        if self.active_user == None:
+            return None
+
+        container = self.user_dict[self.active_user]
+        return container & set(keys)
+
+    def list(self):
+        if self.active_user == None:
+            return None
+
+        container = self.user_dict[self.active_user]
+        return list(container)
+
+    def grep(self, regex_str):
+        if self.active_user == None:
+            return None
+
+        container = self.user_dict[self.active_user]
+        regex = re.compile(regex_str)
+        matched_keys = list()
+
+        for element in container:
+            if regex.fullmatch(value_to_string(element)) != None:
+                matched_keys.append(element)
+
+        return matched_keys
+
+    def save(self, path):
+        if self.active_user == None:
+            return False
+
+        container = self.user_dict[self.active_user]
+
+        with open(path, 'w') as file:
+            for key in container:
+                file.write(value_to_string(key) + '\n')
+
+        return True
+
+    def load(self, path):
+        if self.active_user == None:
+            return False
+
+        container = self.user_dict[self.active_user]
+
+        with open(path) as file:
+            for line in file:
+                container.add(value_from_string(line[:-1]))
+
+        return True
+
+    def switch(self, user):
+        if user not in self.user_dict:
+            self.user_dict[user] = set()
+
+        self.active_user = user
