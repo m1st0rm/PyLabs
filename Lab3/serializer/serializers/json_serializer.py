ea78b0a0ee03cb0928f6a23e0fcc20b33ff9d6ef
m1st0rm <mishaboy333@gmail.com>
2023-06-12 08:55:25 +0300

    Defined class Json, added methods dump and dumps to json_serializer.py

---
+++ Lab3/serializer/serializers/json_serializer.py
@@ -0,0 +1,23 @@
+from serializer.data_packer.packer import Packer
+from .serializer_meta import MetaSerializer
+
+
+class Json(MetaSerializer):
+    data_packer = Packer()
+
+    def dump(self, obj, file):
+        file.write(self.dumps(obj))
+
+    def dumps(self, obj):
+        packed = self.data_packer.pack(obj)
+
+        if isinstance(packed, (list, tuple)):
+            return self.__list_n_tuple_to_string_util(packed)
+
+        if isinstance(packed, dict):
+            return self.__dict_to_string_util(packed)
+
+        return self.__ser_primitive(obj)
+
+
+    
